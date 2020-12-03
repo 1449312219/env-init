531100773544aa0c83fa2f66292d87a44767ac69
kongshuai <kongshuai_jr@dhcc.com.cn>
2020-12-03 09:33:19 +0800

    加入gitea

---
+++ resources/services/1000.gitea/install.sh
@@ -0,0 +1,53 @@
+cd $HELM_DIR/charts
+
+name=gitea
+chart=${name}-0.2.1.tgz
+
+registry=${MASTER_IP}:${DOCKER_REGISTRY_PORT}
+giteaPvcName=${name}-gitea
+postgresPvcName=${name}-postgres
+secretName=${name}-externaldb
+
+if test ${registry} != : ;then
+  set=images.gitea=${registry}/gitea/gitea:1.12.2
+  set=${set},images.postgres=${registry}/postgres:11
+  set=${set},images.memcached=${registry}/memcached:1.5.19-alpine
+fi
+#set=$set,persistence.giteaSize=8Gi,persistence.accessMode=ReadWriteOnce
+
+helm upgrade --install ${name} ${chart} -f values.yaml --set=${set}
+
+
+# 创建PV
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: ${giteaPvcName}
+spec:
+  accessModes:
+  - ReadWriteOnce
+  - ReadWriteMany
+  capacity:
+    storage: 8Gi
+  hostPath:
+    path: /home/vagrant/pv/${giteaPvcName}
+    type: DirectoryOrCreate
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Filesystem
+---
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: ${postgresPvcName}
+spec:
+  accessModes:
+  - ReadWriteMany
+  capacity:
+    storage: 5Gi
+  hostPath:
+    path: /home/vagrant/pv/${postgresPvcName}
+    type: DirectoryOrCreate
+  persistentVolumeReclaimPolicy: Retain
+  volumeMode: Filesystem
+EOF
---
+++ resources/shell/join.sh
@@ -0,0 +1,2 @@
+kubeadm join 192.168.101.10:6443 --token xcqt39.p1qm1fu46x0zq20q \
+    --discovery-token-ca-cert-hash sha256:d4e43649e76d7041af2610e15531a0e99e03cd15d002a67d19270f62c9fd22c7 
